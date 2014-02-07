a472f95c9afaa57d1c9ec232b7e64079ee9a5b41
Silvio Vaz <hello@sevenvisualsigns.com>
2014-02-07 18:11:22 +0000

    updating the paperclip gem data in the model

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,9 @@ class Post < ActiveRecord::Base
 
 	has_attached_file :image, styles: { header: '620x200#' }
 
+	validates_attachment_content_type :image, :content_type => ['image/jpeg', 'image/jpg', 'image/png']
+
+
 	# It returns the Posts whose titles contain one or more words that form the query
 	def self.search(query)
 	    # where(:title, query) -> This would return an exact match of the query
